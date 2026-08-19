7f78336aee8692be3b81da337e6bab709a78b62a
Abro999 <140468810+Abro999@users.noreply.github.com>
2026-08-20 05:13:18 +0530

    Fix indentation for storePassword in signingConfigs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
     create("release") {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
       storeFile = file(keystorePath)
-            storePassword = System.getenv("STORE_PASSWORD") ?: "placeholder"
+      storePassword = System.getenv("STORE_PASSWORD") ?: "placeholder"
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD") ?: "placeholder"
     }
